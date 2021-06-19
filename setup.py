adf39964f48618f1e2f6d6e457d7f9838c06e4be
Andy Doan <andy@foundries.io>
2021-06-18 21:45:27 -0500

    black formatting
    Signed-off-by: Andy Doan <andy@foundries.io>

--- setup.py
+++ setup.py
@@ -7,26 +7,26 @@ from setuptools import find_packages, setup
 
 def read(*parts):
     path = os.path.join(os.path.dirname(__file__), *parts)
-    with codecs.open(path, encoding='utf-8') as fobj:
+    with codecs.open(path, encoding="utf-8") as fobj:
         return fobj.read()
 
 
-install_requires = read('requirements.txt').splitlines()
+install_requires = read("requirements.txt").splitlines()
 
 setup(
-    name='fiodash',
+    name="fiodash",
     version="0.1.0",
     packages=find_packages(),
     install_requires=install_requires,
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
